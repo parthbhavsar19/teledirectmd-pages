27495393f21b833e4fa7194e7836ea9c44056702
parthbhavsar19 <bhavsar.parth25@gmail.com>
2026-03-19 00:37:31 +0000

    Add 50 national condition pages + unified [slug] routing (2,039 pages)

--- app/components/SiteFooter.js
+++ app/components/SiteFooter.js
@@ -27,7 +27,7 @@ export default function SiteFooter() {
           {/* Column 3: Resources */}
           <div className="tdmd-footer-col">
             <h4 className="tdmd-footer-heading">RESOURCES</h4>
-            <a href="/health-guides">Health Guides</a>
+            <a href="/what-we-treat">Health Guides</a>
             <a href="/reviews">Reviews</a>
             <a href="/media-mentions">Media Mentions</a>
           </div>
