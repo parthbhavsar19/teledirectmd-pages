f8a260770284edd0c20dd6661bb0844ccc6222b3
Parth Bhavsar <bhavsar.parth25@gmail.com>
2026-04-13 14:27:45 +0000

    Revert "feat: revamp homepage with canvas scratch animation, 10-section layout"
    This reverts commit 1ce6a53d203ee3de8bdbd3e9fbaa2e64b606408a.

--- app/components/GlobalStyles.js
+++ app/components/GlobalStyles.js
@@ -1,7 +1,6 @@
 import { tdmdCSS } from '../../lib/tdmd-styles';
 import { whatWeTreatCSS } from '../../lib/what-we-treat-styles';
-import { homepageCSS } from '../../lib/homepage-styles';
 
 export default function GlobalStyles() {
-  return <style dangerouslySetInnerHTML={{ __html: tdmdCSS + whatWeTreatCSS + homepageCSS }} />;
+  return <style dangerouslySetInnerHTML={{ __html: tdmdCSS + whatWeTreatCSS }} />;
 }
